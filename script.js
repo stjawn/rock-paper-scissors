f12e052f858305022a50901e1e56c22369e11357
stjawn <stjawn@protonmail.com>
2021-03-15 12:29:07 -0400

    Corrected computerPlay. Created playRound.

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
+let playerHand = prompt("Rock, paper, scissors. Shoot!", "").toLowerCase();
+let computerHand = computerPlay();
+
 function computerPlay() {
-    let randVal = Math.random() % 3;
+    let randVal = ((Math.random() * 10000).toFixed(0)) % 3;
     if (randVal === 0) {
-        return "Rock"
+        return "rock"
     } else if (randVal === 1) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    // *if playerSelection invalid, log message and repeat
+    if (playerSelection === computerSelection) {
+        // *repeat playRound with refreshed variables
+        return console.log("tie")
+    }
+        else if (playerSelection === "rock") {
+            if (computerSelection === "paper") {
+                return console.log("Paper covers rock. You lose!")
+            }
+            else {
+                return console.log("Rock crushes scissors. You win!")
+            }
+        }
+        else if (playerSelection === "paper") {
+            if (computerSelection === "scissors") {
+                return console.log("Scissors cut paper. You lose!")
+            }
+            else {
+                return console.log("Paper covers rock. You win!")
+            }
+        }
+        else {
+            if (computerSelection === "rock") {
+                return console.log("Rock crushes scissors. You lose!")
+            }
+            else {
+                return console.log("Scissors cut paper. You win!")
+            }
+        }
+    }

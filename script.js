58de16912ec01c5badc11879927af5392e9e251c
stjawn <stjawn@protonmail.com>
2021-03-15 15:47:33 -0400

    Changed playRound to return a string and to detect invalid input.

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
-let playerHand = prompt("Rock, paper, scissors. Shoot!", "").toLowerCase();
-let computerHand = computerPlay();
-
 function computerPlay() {
     let randVal = ((Math.random() * 10000).toFixed(0)) % 3;
     if (randVal === 0) {
-        return "rock"
+        return "rock";
     } else if (randVal === 1) {
-        return "paper"
+        return "paper";
     } else {
-        return "scissors"
+        return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    // *if playerSelection invalid, log message and repeat
+    playerSelection = playerSelection.toLowerCase();
+    
     if (playerSelection === computerSelection) {
         // *repeat playRound with refreshed variables
-        return console.log("tie")
+        return "Tie";
     }
         else if (playerSelection === "rock") {
             if (computerSelection === "paper") {
-                return console.log("Paper covers rock. You lose!")
+                return "Paper covers rock. You lose!";
             }
             else {
-                return console.log("Rock crushes scissors. You win!")
+                return "Rock crushes scissors. You win!";
             }
         }
         else if (playerSelection === "paper") {
             if (computerSelection === "scissors") {
-                return console.log("Scissors cut paper. You lose!")
+                return "Scissors cut paper. You lose!";
             }
             else {
-                return console.log("Paper covers rock. You win!")
+                return "Paper covers rock. You win!";
+            }
+        }
+        else if (playerSelection === "scissors") {
+            if (computerSelection === "rock") {
+                return "Rock crushes scissors. You lose!";
+            }
+            else {
+                return "Scissors cut paper. You win!";
             }
         }
         else {
-            if (computerSelection === "rock") {
-                return console.log("Rock crushes scissors. You lose!")
-            }
-            else {
-                return console.log("Scissors cut paper. You win!")
-            }
+            return "You must choose rock, paper, or scissors.";
         }
-    }
+}
+
+
+let playerHand = prompt("Rock, paper, scissors. Shoot!", "");
+let computerHand = computerPlay();
+
+console.log(playerHand);
+console.log(computerHand);
+console.log(playRound(playerHand,computerHand));

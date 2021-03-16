a76fa489d3646fa74038ca6f1945814ee266dcf3
stjawn <stjawn@protonmail.com>
2021-03-16 13:44:53 -0400

    Added game(n) to play n games. Corrected and simplified code by changing return values andremoving variables and parameters. All working.

--- script.js
+++ script.js
@@ -1,54 +1,61 @@
 function computerPlay() {
-    let randVal = ((Math.random() * 10000).toFixed(0)) % 3;
+    let randVal = ((Math.random() * 100000).toFixed(0)) % 3;
     if (randVal === 0) {
-        return "rock";
+        return "rock"
     } else if (randVal === 1) {
-        return "paper";
+        return "paper"
     } else {
-        return "scissors";
+        return "scissors"
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    
-    if (playerSelection === computerSelection) {
-        // *repeat playRound with refreshed variables
-        return "Tie";
+function playRound() {
+    let playerHand = prompt("Rock, paper, scissors. Shoot!","").toLowerCase();
+    let computerHand = computerPlay();
+    if (playerHand === computerHand) {
+        console.log("Tie", playerHand, computerHand);
+        return playRound()
+    } else if (playerHand === "rock") {
+        if (computerHand === "paper") {
+            console.log("Paper covers rock. You lose!");
+            return 0
+        } else {
+            console.log("Rock crushes scissors. You win!");
+            return 1
+        }
+    } else if (playerHand === "paper") {
+        if (computerHand === "scissors") {
+            console.log("Scissors cut paper. You lose!");
+            return 0
+        } else {
+            console.log("Paper covers rock. You win!");
+            return 1
+        }
+    } else if (playerHand === "scissors") {
+        if (computerHand === "rock") {
+            console.log("Rock crushes scissors. You lose!");
+            return 0
+        } else {
+            console.log("Scissors cut paper. You win!");
+            return 1
+        }
+    } else {
+        console.log("You must choose rock, paper, or scissors.");
+        return playRound()
     }
-        else if (playerSelection === "rock") {
-            if (computerSelection === "paper") {
-                return "Paper covers rock. You lose!";
-            }
-            else {
-                return "Rock crushes scissors. You win!";
-            }
-        }
-        else if (playerSelection === "paper") {
-            if (computerSelection === "scissors") {
-                return "Scissors cut paper. You lose!";
-            }
-            else {
-                return "Paper covers rock. You win!";
-            }
-        }
-        else if (playerSelection === "scissors") {
-            if (computerSelection === "rock") {
-                return "Rock crushes scissors. You lose!";
-            }
-            else {
-                return "Scissors cut paper. You win!";
-            }
-        }
-        else {
-            return "You must choose rock, paper, or scissors.";
-        }
 }
 
 
-let playerHand = prompt("Rock, paper, scissors. Shoot!", "");
-let computerHand = computerPlay();
-
-console.log(playerHand);
-console.log(computerHand);
-console.log(playRound(playerHand,computerHand));
+function game(n = 1) {
+    let playerWins = 0;
+    let roundResult = 0;
+    for (let i = 0; i < n; i++) {
+        roundResult = playRound();
+        playerWins += roundResult;
+    }
+    if (playerWins > (n/2)) {
+        return ("Congratulations! You won " + playerWins + " of " + n + " games.")
+    } else {
+        return ("Damn! You lost " + (n - playerWins) + " of " + n + " games.")
+    }
+}

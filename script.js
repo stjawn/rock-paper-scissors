382b9a02584299c17fc8cd0b3053bbbd0f1d3df6
stjawn <stjawn@protonmail.com>
2021-03-24 18:13:18 -0400

    Added functionality to buttons. Added scoreboard. Changed input and output of playRound function.

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+// Constants and Variables
+let wins = 0
+let losses = 0
+let ties = 0 
+let total = wins + losses + ties
+
+
+// User Interface
+
+const mainContainer = document.createElement("div");
+mainContainer.id = "main";
+document.body.appendChild(mainContainer);
+
+const rockButton = document.createElement("button");
+rockButton.id = "rock";
+
+const paperButton = document.createElement("button");
+paperButton.id = "paper";
+
+const scissorsButton = document.createElement("button");
+scissorsButton.id = "scissors";
+
+const buttonContainer = document.createElement("div");
+buttonContainer.id = "buttons"
+buttonContainer.append(rockButton, paperButton, scissorsButton);
+mainContainer.appendChild(buttonContainer);
+
+const scoreContainer = document.createElement("div");
+scoreContainer.id = "scoreboard";
+scoreContainer.textContent = "wins: " + wins + " losses: " + losses + " ties: " + ties
+mainContainer.appendChild(scoreContainer);
+
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.textContent = button.id
+})
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let roundResult = (playRound(button.id));
+        console.log(roundResult)
+        if (roundResult === "tie") {
+          ties += 1;
+        } else if (roundResult === 1) {
+          wins += 1;
+        } else {
+          losses += 1;
+        }
+        scoreContainer.textContent = "wins: " + wins + " losses: " + losses + " ties: " + ties
+    });
+});
+
+
+// Functions
+
 function computerPlay() {
     let randVal = ((Math.random() * 100000).toFixed(0)) % 3;
     if (randVal === 0) {
@@ -9,78 +64,32 @@ function computerPlay() {
     }
 }
 
-function playRound() {
-    let playerHand = prompt("Rock, paper, scissors. Shoot!","").toLowerCase();
-    let computerHand = computerPlay();
+function playRound(playerHand, computerHand = computerPlay()) {
     if (playerHand === computerHand) {
-        console.log("Tie", playerHand, computerHand);
-        return playRound()
+        return "tie"
     } else if (playerHand === "rock") {
         if (computerHand === "paper") {
-            console.log("Paper covers rock. You lose!");
             return 0
         } else {
-            console.log("Rock crushes scissors. You win!");
             return 1
         }
     } else if (playerHand === "paper") {
         if (computerHand === "scissors") {
-            console.log("Scissors cut paper. You lose!");
             return 0
         } else {
-            console.log("Paper covers rock. You win!");
             return 1
         }
     } else if (playerHand === "scissors") {
         if (computerHand === "rock") {
-            console.log("Rock crushes scissors. You lose!");
             return 0
         } else {
-            console.log("Scissors cut paper. You win!");
             return 1
         }
-    } else {
-        console.log("You must choose rock, paper, or scissors.");
-        return playRound()
     }
 }
 
-function game(n = 1) {
-    let playerWins = 0;
-    let roundResult = 0;
-    for (let i = 0; i < n; i++) {
-        roundResult = playRound();
-        playerWins += roundResult;
-    }
-    if (playerWins > (n/2)) {
-        return ("Congratulations! You won " + playerWins + " of " + n + " games.")
-    } else {
-        return ("Damn! You lost " + (n - playerWins) + " of " + n + " games.")
-    }
-}
-
-// User Interface:
-
-const rockButton = document.createElement("button");
-rockButton.id = "rock";
-
-const paperButton = document.createElement("button");
-paperButton.id = "paper";
-
-const scissorsButton = document.createElement("button");
-scissorsButton.id = "scissors";
-
-const btnContainer = document.createElement("div");
-btnContainer.append(rockButton, paperButton, scissorsButton);
-document.body.appendChild(btnContainer);
+ 
+/*
 
 
-
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      return button.id
-    });
-  });
-
-
+*/

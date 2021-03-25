ef7bee21045874ec5069d13a3f34eb6c00f333f0
stjawn <stjawn@protonmail.com>
2021-03-24 21:32:07 -0400

    Added message feature. Simplified eventListener. Changed playRound output.

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 let wins = 0
 let losses = 0
 let ties = 0 
-let total = wins + losses + ties
-
 
 // User Interface
 
 const mainContainer = document.createElement("div");
 mainContainer.id = "main";
 document.body.appendChild(mainContainer);
+
+const scoreContainer = document.createElement("div");
+scoreContainer.id = "scorecontainer";
+scoreContainer.textContent = "wins: " + wins + " losses: " + losses + " ties: " + ties
+mainContainer.appendChild(scoreContainer);
 
 const rockButton = document.createElement("button");
 rockButton.id = "rock";
@@ -21,15 +24,9 @@ const scissorsButton = document.createElement("button");
 scissorsButton.id = "scissors";
 
 const buttonContainer = document.createElement("div");
-buttonContainer.id = "buttons"
+buttonContainer.id = "buttons";
 buttonContainer.append(rockButton, paperButton, scissorsButton);
 mainContainer.appendChild(buttonContainer);
-
-const scoreContainer = document.createElement("div");
-scoreContainer.id = "scoreboard";
-scoreContainer.textContent = "wins: " + wins + " losses: " + losses + " ties: " + ties
-mainContainer.appendChild(scoreContainer);
-
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
@@ -37,19 +34,13 @@ buttons.forEach((button) => {
 })
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        let roundResult = (playRound(button.id));
-        console.log(roundResult)
-        if (roundResult === "tie") {
-          ties += 1;
-        } else if (roundResult === 1) {
-          wins += 1;
-        } else {
-          losses += 1;
-        }
-        scoreContainer.textContent = "wins: " + wins + " losses: " + losses + " ties: " + ties
-    });
-});
+        playRound(button.id);
+    })
+})
 
+const messageContainer = document.createElement("div");
+messageContainer.textContent = "Shoot!";
+mainContainer.appendChild(messageContainer);
 
 // Functions
 
@@ -65,31 +56,55 @@ function computerPlay() {
 }
 
 function playRound(playerHand, computerHand = computerPlay()) {
+    let roundResult
     if (playerHand === computerHand) {
-        return "tie"
+        roundResult = "tie";
     } else if (playerHand === "rock") {
         if (computerHand === "paper") {
-            return 0
+            roundResult = 0;
         } else {
-            return 1
+            roundResult = 1;
         }
     } else if (playerHand === "paper") {
         if (computerHand === "scissors") {
-            return 0
+            roundResult = 0;
         } else {
-            return 1
+            roundResult = 1;
         }
     } else if (playerHand === "scissors") {
         if (computerHand === "rock") {
-            return 0
+            roundResult = 0;
         } else {
-            return 1
+            roundResult = 1;
         }
     }
+    if (roundResult === "tie") {
+        ties += 1;
+        messageContainer.textContent = "Tie!";
+    } else if (roundResult === 1) {
+        wins += 1;
+        messageContainer.textContent = "Wooo, " + playerHand + " beats " + computerHand + ". You win!";
+    } else {
+        losses += 1;
+        messageContainer.textContent = "Damn, " + computerHand + " beats " + playerHand + ". You lose!";
+    }
+    scoreContainer.textContent = "Wins: " + wins + " Losses: " + losses + " Ties: " + ties;
 }
 
- 
+
+
+
 /*
 
+function test(n) {
+    while (n > 0) {
+        playRound("rock");
+        if (n % 100 === 0) {
+            console.log(n);
+        }
+        n -= 1;
+    }
+    console.log("Wins: " + wins + " Losses: " + losses + " Ties: " + ties);
+}
 
 */
